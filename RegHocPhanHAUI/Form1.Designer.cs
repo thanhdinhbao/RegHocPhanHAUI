bf3029475638cb8487bb2e3cfe5ff10e411419fb
Thanh Dinh <thanhdinhbao@outlook.com.vn>
2024-06-13 22:47:12 +0700

    update

--- RegHocPhanHAUI/Form1.Designer.cs
+++ RegHocPhanHAUI/Form1.Designer.cs
@@ -29,61 +29,81 @@ namespace RegHocPhanHAUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.dtModules = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dgvModules = new System.Windows.Forms.DataGridView();
+            this.btnGet = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dtModules)).BeginInit();
+            this.dgvClass = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModules)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClass)).BeginInit();
             this.SuspendLayout();
             // 
-            // dtModules
+            // dgvModules
             // 
-            this.dtModules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtModules.Location = new System.Drawing.Point(1, 52);
-            this.dtModules.Name = "dtModules";
-            this.dtModules.Size = new System.Drawing.Size(581, 329);
-            this.dtModules.TabIndex = 0;
+            this.dgvModules.AllowUserToAddRows = false;
+            this.dgvModules.AllowUserToDeleteRows = false;
+            this.dgvModules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModules.Location = new System.Drawing.Point(12, 41);
+            this.dgvModules.Name = "dgvModules";
+            this.dgvModules.ReadOnly = true;
+            this.dgvModules.Size = new System.Drawing.Size(346, 340);
+            this.dgvModules.TabIndex = 0;
+            this.dgvModules.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtModules_CellContentClick);
             // 
-            // button1
+            // btnGet
             // 
-            this.button1.Location = new System.Drawing.Point(504, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(390, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnGet.Location = new System.Drawing.Point(322, 10);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get Data";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 15);
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(289, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // dgvClass
+            // 
+            this.dgvClass.AllowUserToAddRows = false;
+            this.dgvClass.AllowUserToDeleteRows = false;
+            this.dgvClass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClass.Location = new System.Drawing.Point(376, 41);
+            this.dgvClass.Name = "dgvClass";
+            this.dgvClass.ReadOnly = true;
+            this.dgvClass.Size = new System.Drawing.Size(656, 340);
+            this.dgvClass.TabIndex = 4;
+            this.dgvClass.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClass_CellClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(447, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 386);
+            this.ClientSize = new System.Drawing.Size(1043, 386);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dgvClass);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dtModules);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.dgvModules);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dtModules)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Reg học phần HAUI by ThanhDB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModules)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClass)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,10 +111,11 @@ namespace RegHocPhanHAUI
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dtModules;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvModules;
+        private System.Windows.Forms.Button btnGet;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgvClass;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RegHocPhanHAUI/Form1.cs
+++ RegHocPhanHAUI/Form1.cs
@@ -3,17 +3,48 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace RegHocPhanHAUI
 {
     public partial class Form1 : Form
     {
+        private DataTable dataTable;
+        private BindingSource bindingSource;
+
+        private DataTable dtClass;
+        private BindingSource bSClass;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private DataTable ConvertToDataTable<T>(List<T> items)
+        {
+            var dataTable = new DataTable(typeof(T).Name);
+
+            // Get all the properties
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo prop in props)
+            {
+                // Setting column names as Property names
+                dataTable.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (T item in items)
+            {
+                var values = new object[props.Length];
+                for (int i = 0; i < props.Length; i++)
+                {
+                    // Inserting property values to datatable rows
+                    values[i] = props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
 
         async void GetAllModules()
         {
@@ -41,12 +72,21 @@ namespace RegHocPhanHAUI
             RestResponse response = await client.ExecuteAsync(request);
             List<Modules> parsed_json = JsonConvert.DeserializeObject<List<Modules>>(response.Content);
 
-            // Bind the DataTable to the DataGridView
-            dtModules.DataSource = parsed_json;
+            // Convert the list to a DataTable
+            dataTable = ConvertToDataTable(parsed_json);
+
+            // Initialize BindingSource and set the DataSource to the DataTable
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = dataTable;
+
+            // Set the DataGridView DataSource to the BindingSource
+            dgvModules.DataSource = bindingSource;
+
+            textBox1.TextChanged += textBox1_TextChanged;
 
         }
 
-        async void GetClassByModules()
+        async void GetClassByModules(string fid)
         {
             var options = new RestClientOptions()
             {
@@ -68,10 +108,79 @@ namespace RegHocPhanHAUI
             request.AddHeader("sec-fetch-mode", "cors");
             request.AddHeader("sec-fetch-site", "same-origin");
             request.AddHeader("x-requested-with", "XMLHttpRequest");
-            var body = @"fid=4834";
-            request.AddParameter("application/x-www-form-urlencoded; charset=UTF-8", body, ParameterType.RequestBody);
+            var body = @"fid="+ fid;
+            request.AddParameter("application/x-www-form-urlencoded", body, ParameterType.RequestBody);
             RestResponse response = await client.ExecuteAsync(request);
-            Console.WriteLine(response.Content);
+            ClassByModules.Root parsed_json = JsonConvert.DeserializeObject<ClassByModules.Root>(response.Content);
+
+            bSClass = new BindingSource();
+            bSClass.DataSource = parsed_json.data;
+
+            // Set the DataGridView DataSource to the BindingSource
+            dgvClass.DataSource = bSClass;
+
+            textBox2.TextChanged += textBox2_TextChanged;
+
+
+        }
+
+        async void AddClass(string ClassID)
+        {
+            var options = new RestClientOptions()
+            {
+                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("https://sv.haui.edu.vn/ajax/register/action.htm?cmd=addclass&v=F9440123FCF48CBB13A5184614B8362E", Method.Post);
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddHeader("accept-language", "en-US,en;q=0.9,vi;q=0.8,ar;q=0.7,de;q=0.6");
+            request.AddHeader("content-length", "0");
+            request.AddHeader("cookie", "_ga_G1VDE91S1Z=GS1.3.1687399002.1.0.1687399002.0.0.0; UqZBpD3n=v1Uh+GSQ__YGU; kVisit=5d6d9522-e4fc-4e1a-a007-23b9d503e36d; _ga_ZG0RX9N53W=GS1.3.1691194734.2.1.1691194937.0.0.0; _ga_61721GVZDB=GS1.1.1691810371.1.0.1691810376.55.0.0; __Host-UqZBpD3n=v1Uh+GSQ__YGU; _ga=GA1.1.1106234729.1687399002; _ga_M6W50XGDVZ=GS1.1.1718155398.13.1.1718156204.0.0.0; _ga_N1ND2KBTYH=GS1.1.1718155400.13.1.1718156204.0.0.0; ASP.NET_SessionId=ciifsj3hogew2ro3x1isgjbe; onehauisv=F878D39A967227A3512895F4699619751E871EAFF666620F2192B52440078DD84207661024E9D5D8E50B87264DC8354B268586686E8699A50CDF639FC84551A05324630B240A83698154E88E0F187F42D51238D71B3315C5CED9EDD79AFDBF57685468C82A123D44AD79D8F62733C95B02BFA1765F45D942E08BDA47C57FA64F4E5FFDA91ED343BF2776C81A82C261FB5BB8FE9BCA518109BD3A9F3C74146FE0B9D8DED89F9C4C3BE8E55861A6C8AE007811CB51551256590ADCE3D4DC43B3399E9E5046E2F47173C304D57F219801E5E041234BD93B2C3D26B6370317FE3ACB7323D3E87D2BF132E946F02D108399AABDCB0C4AB96139CCF863AFE3BB4EB0A041548E649108BFADD31BA43437FF46736672A22003C5B3BD74F0B10C11F22CD81F4DEB7B4FB95B47CFF745832E01E26F7E99899C4DDA1DE511093D92319FDDB8EBAFD4DD1FEDC4DCDFD2A1D481E393D918C58820F0F88A65614348EDA93C4EEF11C22BC2A927ED7F47E01E1F181A8D07; _ga_S8WJEW3D2H=GS1.1.1718276018.20.1.1718276221.0.0.0; __Host-UqZBpD3n=v1Uh+GSQ__YGU");
+            request.AddHeader("origin", "https://sv.haui.edu.vn");
+            request.AddHeader("priority", "u=1, i");
+            request.AddHeader("referer", "https://sv.haui.edu.vn/register/");
+            request.AddHeader("sec-ch-ua", "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"");
+            request.AddHeader("sec-ch-ua-mobile", "?0");
+            request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
+            request.AddHeader("sec-fetch-dest", "empty");
+            request.AddHeader("sec-fetch-mode", "cors");
+            request.AddHeader("sec-fetch-site", "same-origin");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("class", ClassID);
+            request.AddParameter("ctdk", "886");
+            RestResponse response = await client.ExecuteAsync(request);
+            MessageBox.Show(response.Content);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Apply filter to the BindingSource based on the TextBox input
+            string filter = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                bindingSource.RemoveFilter();
+            }
+            else
+            {
+                // Filter the data by the "Name" column
+                bindingSource.Filter = $"ModulesName LIKE '%{filter}%'";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Apply filter to the BindingSource based on the TextBox input
+            string filter = textBox2.Text;
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                bSClass.RemoveFilter();
+            }
+            else
+            {
+                // Filter the data by the "Name" column
+                bSClass.Filter = $"ClassCode LIKE '%{filter}%'";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -79,26 +188,40 @@ namespace RegHocPhanHAUI
             GetAllModules();
         }
 
-
-
-        private void button2_Click(object sender, EventArgs e)
+        private void dtModules_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            try
-            {
-                string searchValue = textBox1.Text;
-                string colName = dtModules.Columns[1].Name;//Column Number of Search
-
-                ((DataTable)dtModules.DataSource).DefaultView.RowFilter = string.Format(colName + " like '%{0}%'", searchValue.Trim().Replace("'", "''"));
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message);
-            }
+            string fid = dgvModules.Rows[e.RowIndex].Cells[0].Value.ToString();
+            GetClassByModules(fid);
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            button2_Click(null, null);
+            //dgvClass.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dgvModules.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            DataGridViewButtonColumn btnColumn = new DataGridViewButtonColumn();
+            btnColumn.HeaderText = "Đăng kí";
+            btnColumn.Name = "btnColumn";
+            btnColumn.Text = "Đăng kí";
+            btnColumn.UseColumnTextForButtonValue = true; // Hiển thị text trên nút
+
+            // Thêm cột vào DataGridView
+            dgvClass.Columns.Add(btnColumn);
+        }
+        private void ButtonClickHandler(int rowIndex)
+        {
+            int idx = dgvClass.CurrentCell.RowIndex;
+            string ClassID = dgvClass.Rows[idx].Cells[1].Value.ToString();
+            AddClass(ClassID);
+            
+        }
+
+        private void dgvClass_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvClass.Columns["btnColumn"].Index && e.RowIndex >= 0)
+            {
+                // Gọi hàm xử lý khi nút được nhấn
+                ButtonClickHandler(e.RowIndex);
+            }
         }
     }
 }
